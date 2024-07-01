e14afc8b92e42f2140d6473035ed3b4fe85549e2
mham <warofzen1@proton.me>
2024-07-01 19:28:39 +0700

    fix virtmanager

--- system/features/programs/virt-manager.nix
+++ system/features/programs/virt-manager.nix
@@ -2,12 +2,5 @@
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
 
-  dconf.settings = {
-    "org/virt-manager/virt-manager/connections" = {
-      autoconnect = ["qemu:///system"];
-      uris = ["qemu:///system"];
-    };
-  };
-
   users.users.mham.extraGroups = ["libvirtd"];
 }
